b7aa8633032fe46b9430e50897210f2a0b8e7fe9
alkachhirang <alkachhirang3@gmail.com>
2024-05-10 11:32:17 +0530

    'updates'

--- assets/js/app.js
+++ assets/js/app.js
@@ -176,3 +176,15 @@ Submit.addEventListener("click", function (event) {
 const currentDate = new Date();
 const currentYear = currentDate.getFullYear();
 document.getElementById("currentYear").textContent = currentYear;
+// back to top
+function backtotop() {
+    window.scroll(0, 0);
+}
+window.addEventListener("scroll", function () {
+    let backtotop = document.getElementById("backtotop");
+    if (window.scrollY > 250) {
+        backtotop.style.display = "block";
+    } else {
+        backtotop.style.display = "none";
+    }
+});
