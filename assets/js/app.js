8e2122689571d372daaf2800e785a60ff27c5ff2
alkachhirang <alkachhirang3@gmail.com>
2024-05-11 22:29:16 +0530

    'changes'

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,10 @@
+const form = document.getElementById('headerfrom');
+const submitButton = document.querySelector('.submitheaderfrom');
+submitButton.addEventListener('mouseover', () => {
+  const isFormValid = Array.from(form.elements).every(input => input.validity.valid);
+  submitButton.style.cursor = isFormValid ? 'pointer' : 'not-allowed';
+});
+
 //================header-timeout===============
 function countdown(endDate) {
   //  current date and time
