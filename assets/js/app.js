a30ece0a5591fd0264b6fb7314e4fa14c74e1af0
alkachhirang <alkachhirang3@gmail.com>
2024-05-12 13:58:27 +0530

    changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,6 +21,22 @@ tradingsubmitButton.addEventListener('mouseover', () => {
   tradingsubmitButton.style.cursor = tradingform.classList.contains('invalid') ? 'not-allowed' : 'pointer';
 });
 
+
+var myform = document.getElementById("headerfrom");
+var mysubmitButton = document.getElementById("submit");
+myform.addEventListener("input", function () {
+  // Check if all required fields are filled
+  if (myform.checkValidity()) {
+    // If all fields are filled, enable the submit button
+    mysubmitButton.disabled = false;
+    mysubmitButton.style.cursor = "pointer";
+  } else {
+    // If any required field is empty, disable the submit button
+    mysubmitButton.disabled = true;
+    mysubmitButton.style.cursor = "not-allowed";
+  }
+});
+
 //================header-timeout===============
 function countdown(endDate) {
   //  current date and time
